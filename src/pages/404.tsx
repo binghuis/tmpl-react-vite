56fb3b3526ee9db8252251249d5c30c9c3862261
songbinghui <soongbinghui@gmail.com>
2023-05-06 19:23:53 +0800

    feat: login & 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { Link } from "@/router";
+
+const DemoPage = () => {
+    return <div >
+        <Link to={'/demo'}>404</Link>
+    </div>;
+  };
+  export default DemoPage;
---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import { Link } from "@/router";
+
+const DemoPage = () => {
+    return <div >
+        <Link to={'/demo'}>login</Link>
+    </div>;
+  };
+  export default DemoPage;
