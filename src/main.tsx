d052937de3d8592743c0d640ac77bf70fdb95155
songbinghui <soongbinghui@gmail.com>
2023-05-08 15:48:10 +0800

    feat: 国际化中文

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,40 @@ import { ConfigProvider, ThemeConfig, theme } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 const { compactAlgorithm } = theme;
+import { blue } from '@ant-design/colors';
 import { Routes } from '@generouted/react-router';
 
 import './index.css';
 import 'antd/dist/reset.css';
 
+import zhCN from 'antd/locale/zh_CN';
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
+const defaultData: ThemeData = {
+  borderRadius: 2,
+  colorPrimary: blue[4],
+};
+
 const themeConfig: ThemeConfig = {
   algorithm: [compactAlgorithm],
-  token: {},
-  components: {
-    Button: {},
+  token: {
+    colorPrimary: defaultData.colorPrimary,
+    borderRadius: defaultData.borderRadius,
   },
+};
+
+type ThemeData = {
+  borderRadius: number;
+  colorPrimary: string;
 };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StyleProvider hashPriority="high">
-      <ConfigProvider theme={themeConfig}>
+      <ConfigProvider locale={zhCN} theme={themeConfig}>
         <Routes />
       </ConfigProvider>
     </StyleProvider>
