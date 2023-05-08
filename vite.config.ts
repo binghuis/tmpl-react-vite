a894954c7785d9820f6bc06ff33377621b4f343e
songbinghui <soongbinghui@gmail.com>
2023-05-08 13:23:22 +0800

    refactor: test

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted()],
-  resolve: { alias: { '@': '/src' } },
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        changeOrigin: true,
-      },
-    },
-  },
+	plugins: [react(), generouted()],
+	resolve: { alias: { '@': '/src' } },
+	server: {
+		host: true,
+		proxy: {
+			'/api': {
+				target: 'http://',
+				rewrite: (path) => path.replace(/^\/api/, ''),
+				changeOrigin: true,
+			},
+		},
+	},
 });
