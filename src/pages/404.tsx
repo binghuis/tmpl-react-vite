6bf4002f5969f6bef524555da2c48224980ae14b
songbinghui <soongbinghui@gmail.com>
2023-05-26 10:33:49 +0800

    feat: 404,500 navigate

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,27 @@
+import { useNavigate } from '@/router';
 import { Button, Result } from 'antd';
 import React from 'react';
 
-const Page404: React.FC = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="抱歉，你访问的路径不存在"
-    extra={
-      <Button type="primary" size="small">
-        返回首页
-      </Button>
-    }
-  />
-);
+const Page404: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，你访问的路径不存在"
+      extra={
+        <Button
+          onClick={() => {
+            navigate('/');
+          }}
+          type="primary"
+          size="small"
+        >
+          返回首页
+        </Button>
+      }
+    />
+  );
+};
 
 export default Page404;
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,17 +1,28 @@
+import { useNavigate } from '@/router';
 import { Button, Result } from 'antd';
 import React from 'react';
 
-const Page404: React.FC = () => (
-  <Result
-    status="500"
-    title="500"
-    subTitle="抱歉，服务器出错了"
-    extra={
-      <Button type="primary" size="small">
-        返回首页
-      </Button>
-    }
-  />
-);
+const Page404: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Result
+      status="500"
+      title="500"
+      subTitle="抱歉，服务器出错了"
+      extra={
+        <Button
+          onClick={() => {
+            navigate('/');
+          }}
+          type="primary"
+          size="small"
+        >
+          返回首页
+        </Button>
+      }
+    />
+  );
+};
 
 export default Page404;
