d79c79f510ef841f5ace5e75e3fd1f04d33666ac
songbinghui <soongbinghui@gmail.com>
2023-08-03 16:28:45 +0800

    feat: home navigate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Container from '@/components/container';
 import Page from '@/components/page';
 
 const PageHome = () => {
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   navigate('/');
+  // }, []);
   return (
     <Page title="Home">
       <Container title="首页">Home</Container>
